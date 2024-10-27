1fc4f108859119ad4102483883592edd39d3e770
aanos-m <aanosmahmood@yahoo.com>
2024-10-26 21:31:27 -0500

    dependency setup

--- backend/src/main/java/com/aanos/backend/BackendApplication.java
+++ backend/src/main/java/com/aanos/backend/BackendApplication.java
@@ -1,9 +1,13 @@
 package com.aanos.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
